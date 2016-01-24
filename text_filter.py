5e74bfcf03afdc0eaf58c668e063fd41432b6da0
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-01-24 00:02:10 +0000

    Add text filter and text filter composite (pipeline)

---
+++ text_filter.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta, abstractmethod
+
+class TextFilter(object, metaclass=ABCMeta):
+    """ An intertface for text filters 
+
+        Only has one method: apply() which recieves a string as 
+        an argument and returns a string. 
+    """
+
+    @abstractmethod
+    def apply(self, text):
+        """ Recieves a string, filters it, and returns a string. """
+        pass
+
+class Pipeline(object):
+    """ A composite TextFilter class 
+
+        Uses the Composite Pattern with TextFilters
+    """
+
+    def __init__(self, filters=None):
+        self._filters = []
+
+        if filters is not None:
+            for fil in filters:
+                self.add_filter(fil)
+
+    def add(self, fil):
+        if isinstance(filter, TextFilter):
+            raise TypeError("fil must be a subclass of TextFilter")
+        self._filters += [fil]
+        return self # allow chained additions
+
+    def apply(self, string):
+        result = string
+        for fil in self._filters:
+            result = fil.apply(result)
+        return result
